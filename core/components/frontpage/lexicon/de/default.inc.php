ee01d300f3a4a1f80f6a4ea1f38b681e53472027
Steve Hamblett <steve.hamblett@linux.com>
2011-05-18 11:01:21 +0100

    Signed-off-by: Steve Hamblett <steve.hamblett@linux.com>

---
+++ core/components/frontpage/lexicon/de/default.inc.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Frontpage lexicon
+ *
+ * @category  Front end editing
+ * @author    Thomas Heuer <projekte@thomas-heuer.eu>
+ * @license   GPLv3 http://www.gnu.org/licenses/gpl.html
+ * @language  de
+ *
+ * @package   frontpage
+ * @subpackage lexicon
+ */
+$_lang['frontpage'] = 'Frontpage';
+$_lang['createbutton'] = 'Erstellen';
+$_lang['editbutton'] = 'Bearbeiten';
+$_lang['nosuchdocument'] = '<p style="font-size: 1.5em; text-align:center; margin-left:5%; color:red;">Das von Ihnen angeforderte Dokument wurde nicht gefunden</p>';
+$_lang['cantsave'] = '<p style="font-size: 1.5em; text-align:center; margin-left:5%; color:red;">Speichern des Dokuments fehlgeschlagen</p>';
+$_lang['cantcreate'] = '<p style="font-size: 1.5em; text-align:center; margin-left:5%; color:red;">Erstellen des Dokuments fehlgeschlagen</p>';
+$_lang['cantgetparent'] = '<p style="font-size: 1.5em; text-align:center; margin-left:5%; color:red;">Konnte übergeordnetes Dokument nicht finden</p>';
+$_lang['titlelabel'] = 'Titel:';
+$_lang['longtitlelabel'] = 'Langer Titel:';
+$_lang['descriptionlabel'] = 'Beschreibung:';
+$_lang['aliaslabel'] = 'Ressourcen-Alias:';
+$_lang['summarylabel'] = 'Zusammenfassung:';
+$_lang['menutitlelabel'] = 'Menü-Titel:';
+$_lang['menuindexlabel'] = 'Menü-Index:';
+$_lang['hidemenulabel'] = 'Nicht in Menüs anzeigen';
+$_lang['publishlabel'] = 'Veröffentlicht';
+$_lang['folderlabel'] = 'Ordner';
+$_lang['savebuttonlabel'] = 'Speichern';
